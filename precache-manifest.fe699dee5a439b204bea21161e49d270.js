96b9a2118dbe5c75a0217d307b1e2377a46f1d6c
Nguyễn Văn Anh <97245425+nguyenvananhit@users.noreply.github.com>
2022-03-25 09:56:51 +0700

    update

---
+++ precache-manifest.fe699dee5a439b204bea21161e49d270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837d66a5b0e058f936d4478f4544cdd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bb159b2d1ebdca038dd",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "3c66d25d09341ca08182",
+    "url": "/static/js/2.8a7407c5.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.8a7407c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb159b2d1ebdca038dd",
+    "url": "/static/js/main.cabb08d7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
